558df8aa1691adb4171c059dc153bdb3aa59f361
Lawrence Ching <lawrence.ching.zh@gmail.com>
2020-12-20 10:33:25 +0800

    Print HTTP headers and content in WebHookHandler

--- notification/src/main/java/me/imlc/notification/WebHookHandler.java
+++ notification/src/main/java/me/imlc/notification/WebHookHandler.java
@@ -1,21 +1,29 @@
 package me.imlc.notification;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerRequest.Headers;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class WebHookHandler {
 
   public Mono<ServerResponse> gitee(ServerRequest request) {
 
+    Headers headers = request.headers();
+    headers.asHttpHeaders().entrySet().forEach(e -> {
+      log.info("{}: {}", e.getKey(), e.getValue());
+    });
+
     return request.bodyToMono(String.class)
-        .log()
         .map((body) -> {
+          log.info(body);
           return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
               .body(BodyInserters.fromValue(""));
         })
